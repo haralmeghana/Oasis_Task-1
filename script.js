ffde4cb80d234979e5a7d7996954197649ccab5e
haralmeghana <128888658+haralmeghana@users.noreply.github.com>
2023-08-02 21:47:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+function Aaa(){
+    document.getElementById("p").src="./images/watch1--2.jpeg";
+}
+function Bbb(){
+    document.getElementById("p").src="./images/watch1--1.jpeg";
+}
+function Aa(){
+    document.getElementById("meg").src="./images/watch2--2.jpeg";
+}
+function Bb(){
+    document.getElementById("meg").src="./images/watch2--1.jpeg";
+}
+function Aa1(){
+    document.getElementById("gha").src="./images/watch3--2.jpeg";
+}
+function Bb1(){
+    document.getElementById("gha").src="./images/watch3--1.jpeg";
+}
+function Aa2(){
+    document.getElementById("ana").src="./images/watch4--2.jpeg";
+}
+function Bb2(){
+    document.getElementById("ana").src="./images/watch4--1.jpeg";
+}
